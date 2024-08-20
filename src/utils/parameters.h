d82f22a9195b5fb744fbb60f14f8ef27e1638725
Quézia Moura <81170492+QueziaMoura@users.noreply.github.com>
2024-08-20 10:56:42 -0300

    Declaração das constantes a1,a2 e kl

--- src/utils/parameters.h
+++ src/utils/parameters.h
@@ -14,4 +14,8 @@ const float I_yy = 16.0e-6; // kg.m^2
 const float I_zz = 29.0e-6; // kg.m^2
 const float l = 33.0e-3;    // m
 
+const float a1 = 2.4234e-10;    // Cte que multiplica w  (p2)
+const float a2 = 1.4902e-07;    // Cte que multiplica w² (p1)
+
+const float kl =1.8798e-08;    // Cte de sustentação (Cte que multiplica w²) (p1)
 #endif
