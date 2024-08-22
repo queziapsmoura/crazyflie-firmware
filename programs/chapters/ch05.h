46d3a242da3fcd5d1fd04a4b51f2a6907c932edb
Quézia Moura <81170492+QueziaMoura@users.noreply.github.com>
2024-08-22 09:02:41 -0300

    Identificação da constante de arrasto

---
+++ programs/chapters/ch05.h
@@ -0,0 +1,42 @@
+#include "mbed.h"
+#include "crazyflie.h"
+
+PwmOut motor1(MOTOR1);
+PwmOut motor2(MOTOR2);
+PwmOut motor3(MOTOR3);
+PwmOut motor4(MOTOR4);
+
+// Converts desired angular velocity (rad/s) to PWM signal (%)
+float control_motor(float omega)
+{
+    float motor = (omega*omega)*a2 + (omega)*a1;
+    return motor;
+}
+
+// Main program
+int main()
+{
+    // Set PWM frequency to 500Hz
+    wait(2.0);
+    motor1.period(1.0/500.0);
+    motor2.period(1.0/500.0);
+    motor3.period(1.0/500.0);
+    motor4.period(1.0/500.0);
+
+    // Turn on motor
+    motor1 = control_motor(1000.0);
+    motor3 = control_motor(1000.0);
+
+    motor2 = control_motor(2000.0);
+    motor4 = control_motor(2000.0);
+
+    wait(2.0);
+    motor1 = 0.0;
+    motor2 = 0.0;
+    motor3 = 0.0;
+    motor4 = 0.0;
+    while(true)
+    {
+    }
+}
+
