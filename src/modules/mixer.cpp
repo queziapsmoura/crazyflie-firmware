5256bc6936cd030c399b7397dbabe62b66c6c5d0
Quézia Moura <81170492+QueziaMoura@users.noreply.github.com>
2024-09-03 10:24:47 -0300

    Atualização de comentários - Armed

--- src/modules/mixer.cpp
+++ src/modules/mixer.cpp
@@ -11,8 +11,8 @@ Mixer::Mixer():motor_1(MOTOR1), motor_2(MOTOR2), motor_3(MOTOR3), motor_4(MOTOR4
     motor_3 = 0.0;
     motor_4 = 0.0;
     armed =  false;
-    led_red = false;
-    led_green = true;
+    led_red = false;   // Desligado - NF
+    led_green = true;  // Ligado - NA
 }
 void Mixer:: actuate(float f_t, float thau_phi, float thau_theta, float thau_psi)
 {
@@ -77,7 +77,6 @@ float Mixer::control_motor(float omega)
 }
 
 void Mixer:: arm(){
-    armed = true;
 
     //melodia do MARIO THEME
     int melodia[] = {660,660,660,510,660,770,380,510,380,320,440,480,450,430,380,660,760,860,700,760,660,520,580,480,510,380,320,440,480,450,430,380,660,760,860,700,760,660,520,580,480,500,760,720,680,620,650,380,430,500,430,500,570,500,760,720,680,620,650,1020,1020,1020,380,500,760,720,680,620,650,380,430,500,430,500,570,585,550,500,380,500,500,500,500,760,720,680,620,650,380,430,500,430,500,570,500,760,720,680,620,650,1020,1020,1020,380,500,760,720,680,620,650,380,430,500,430,500,570,585,550,500,380,500,500,500,500,500,500,500,580,660,500,430,380,500,500,500,500,580,660,870,760,500,500,500,500,580,660,500,430,380,660,660,660,510,660,770,380};
@@ -87,10 +86,9 @@ void Mixer:: arm(){
     //pausa depois das notas
     int pausadepoisdasnotas[] ={150,300,300,100,300,550,575,450,400,500,300,330,150,300,200,200,150,300,150,350,300,150,150,500,450,400,500,300,330,150,300,200,200,150,300,150,350,300,150,150,500,300,100,150,150,300,300,150,150,300,150,100,220,300,100,150,150,300,300,300,150,300,300,300,100,150,150,300,300,150,150,300,150,100,420,450,420,360,300,300,150,300,300,100,150,150,300,300,150,150,300,150,100,220,300,100,150,150,300,300,300,150,300,300,300,100,150,150,300,300,150,150,300,150,100,420,450,420,360,300,300,150,300,150,300,350,150,350,150,300,150,600,150,300,350,150,150,550,325,600,150,300,350,150,350,150,300,150,600,150,300,300,100,300,550,575};
 
-
-    //for para tocar as 156 notas começando no 0 ate 156 ++ incrementado
-    // for para tocar as 156 notas começando no 0 ate 6 ++ incrementado - Toque Inicial
-    for (int nota = 0; nota < 6; nota++) {
+    //for para tocar as 156 notas começando no 0 ate 156 ++ incrementado - Música Completa
+    // for para tocar as 6 notas começando no 0 ate 6 ++ incrementado - Toque Inicial
+    for (int nota = 0; nota < 156; nota++) {
         float duracaodanota = duracaodasnotas[nota]/2000.00;
         float pausa = pausadepoisdasnotas[nota]/2000.00;
         float periodo = melodia[nota];
@@ -116,12 +114,16 @@ void Mixer:: arm(){
         led_red = !led_red;
         wait(0.25);  
     }
+    armed = true;
+    // Led vermelho - ligado
     led_red = false;
 }
 
 void Mixer:: disarm(){
     actuate(0, 0, 0, 0);
+    // Led vermelho - Desligada 
     led_red = true;
+    // Led verde - ligado
     led_green = false;
     armed = false;
 }
