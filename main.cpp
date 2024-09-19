ef7ed9a2b184c76766678ca2483d1da79e7e7b9f
Quézia Moura <81170492+QueziaMoura@users.noreply.github.com>
2024-09-19 08:31:10 -0300

    Estimator de Attitude

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 // #include "programs/examples/ex01_leds.h"
 //#include "programs/examples/ex02_motors.h"
 // #include "programs/examples/ex03_serial.h"
-// #include "programs/examples/ex04_imu.h"
+#include "programs/examples/ex04_imu.h"
 // #include "programs/examples/ex05_range.h"
 // #include "programs/examples/ex06_flow.h"
 
@@ -14,5 +14,5 @@
 // #include "programs/chapters/ch05.h"
 // #include "programs/chapters/ch07_mixer.h"
 // #include "programs/chapters/ch07_mixer.h"
-#include "programs/chapters/ch09.h"
+// #include "programs/chapters/ch09.h"
 
