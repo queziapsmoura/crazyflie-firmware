147b17253b0868c801aabb7d4bf22946878df10b
Quézia Moura <81170492+QueziaMoura@users.noreply.github.com>
2024-09-19 08:29:44 -0300

    Criação dos Módulos de Attitude

---
+++ src/modules/attitude_estimator.h
@@ -0,0 +1,26 @@
+# ifndef attitude_estimator_h
+# define attitude_estimator_h
+
+# include "mbed.h"
+# include "crazyflie.h"
+
+// Attitude estimator class
+class AttitudeEstimator
+{
+    public :
+        // Class constructor
+        AttitudeEstimator () ;
+        // Initialize class
+        void init () ;
+        // Estimate Euler angles ( rad ) and angular velocities ( rad /s)
+        void estimate () ;
+        // Euler angles (rad)
+        float phi , theta , psi ;
+        // Angular velocities ( rad /s)
+        float p, q, r;
+    private :
+        // IMU sensor object
+        BMI088 imu;
+ };
+
+# endif
