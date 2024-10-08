f892dce02657790082b1d91a05f617bb53fc0fa7
Quézia Moura <81170492+QueziaMoura@users.noreply.github.com>
2024-10-08 08:24:54 -0300

    Comentários Estimador

--- src/modules/attitude_estimator.cpp
+++ src/modules/attitude_estimator.cpp
@@ -2,6 +2,15 @@
 // Class constructor
 AttitudeEstimator::AttitudeEstimator():imu( IMU_SDA , IMU_SCL )
 {
+    phi = 0.0;
+    theta = 0.0;
+    psi = 0.0;
+    p = 0.0;
+    q = 0.0;
+    r = 0.0;
+    imu.ax = 0.0;
+    imu.ay = 0.0;
+
 }
 // Initialize class
 void AttitudeEstimator::init ()
@@ -12,6 +21,8 @@ void AttitudeEstimator::init ()
 void AttitudeEstimator::estimate ()
 {
     imu.read();
+    // phi_a : phi do aceçer}ometro
+    // atan2: Para saber em qual quadrante o ângulo está
     float phi_a = atan2(-imu.ay,-imu.az);
     phi = (1-alpha)*phi + alpha*phi_a;
 }
