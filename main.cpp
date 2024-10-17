4e46fe6beb88e68ffe61725c463e8b4da3c49a6e
ViniciusGallo <vinigallo@outlook.com>
2024-10-17 08:25:22 -0300

    .

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 //#include "programs/examples/ex02_motors.h"
 // #include "programs/examples/ex03_serial.h"
 // #include "programs/examples/ex04_imu.h"
-// #include "programs/examples/ex05_range.h"
+#include "programs/examples/ex05_range.h"
 // #include "programs/examples/ex06_flow.h"
 
 // Chapters
@@ -15,5 +15,5 @@
 // #include "programs/chapters/ch07_mixer.h"
 // #include "programs/chapters/ch07_mixer.h"
 // #include "programs/chapters/ch09.h"
-#include "programs/chapters/ch15.h"
+//#include "programs/chapters/ch15.h"
 // #include "programs/chapters/ch17.h"
